41994bb4170dd73910390a2157c49ce0d2e009bf
twember <wembertim@gmail.com>
2020-12-30 12:50:18 +0100

    Detail Ansicht

--- app/src/main/java/de/gwtgteam/gowiththegrow/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/de/gwtgteam/gowiththegrow/ui/dashboard/DashboardFragment.java
@@ -1,20 +1,25 @@
 package de.gwtgteam.gowiththegrow.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import de.gwtgteam.gowiththegrow.DetailActivity;
 import de.gwtgteam.gowiththegrow.R;
+import de.gwtgteam.gowiththegrow.ui.wiki.AZFragment;
 
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends Fragment implements AdapterView.OnClickListener{
 
     private DashboardViewModel dashboardViewModel;
 
@@ -23,6 +28,9 @@ public class DashboardFragment extends Fragment {
         dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        ConstraintLayout layout = root.findViewById(R.id.DashboardElement);
+        layout.setOnClickListener(this);
         /*final TextView textView = root.findViewById(R.id.text_dashboard);
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -31,5 +39,13 @@ public class DashboardFragment extends Fragment {
             }
         });*/
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Then you start a new Activity via Intent
+        Intent intent = new Intent();
+        intent.setClass(this.getContext(), DetailActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/de/gwtgteam/gowiththegrow/ui/wiki/WikiFragment.java
+++ app/src/main/java/de/gwtgteam/gowiththegrow/ui/wiki/WikiFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
