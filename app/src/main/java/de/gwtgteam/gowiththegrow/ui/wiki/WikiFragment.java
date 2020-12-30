bae5f2dc714a957ddfabd4405d13674e63aac890
Lisa Brundert <lisa.brundert@gmx.de>
2020-12-30 11:20:33 +0100

    wiki list

--- app/src/main/java/de/gwtgteam/gowiththegrow/ui/wiki/WikiFragment.java
+++ app/src/main/java/de/gwtgteam/gowiththegrow/ui/wiki/WikiFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +13,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import java.util.ArrayList;
 
 import de.gwtgteam.gowiththegrow.R;
 
@@ -23,11 +27,23 @@ public class WikiFragment extends Fragment {
         wikiViewModel =
                 new ViewModelProvider(this).get(WikiViewModel.class);
         View root = inflater.inflate(R.layout.fragment_wiki, container, false);
-        final TextView textView = root.findViewById(R.id.text_wiki);
+        ListView listView = (ListView) root.findViewById(R.id.list_wiki);
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Test1");
+        list.add("Test1");
+        list.add("Test1");
+        list.add("Test1");
+        list.add("Test1");
+        list.add("Test1");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1, list);
+        listView.setAdapter(arrayAdapter);
+
         wikiViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //textView.setText(s);
             }
         });
         return root;
