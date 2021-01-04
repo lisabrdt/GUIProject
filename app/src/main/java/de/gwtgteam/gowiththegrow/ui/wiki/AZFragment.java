27fe7da228550e24ea62b8c23efc24082a9a7a90
Lisa Brundert <lisa.brundert@gmx.de>
2021-01-04 14:27:22 +0100

    fixed header

--- app/src/main/java/de/gwtgteam/gowiththegrow/ui/wiki/AZFragment.java
+++ app/src/main/java/de/gwtgteam/gowiththegrow/ui/wiki/AZFragment.java
@@ -1,22 +1,29 @@
 package de.gwtgteam.gowiththegrow.ui.wiki;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 import de.gwtgteam.gowiththegrow.DetailActivity;
 import de.gwtgteam.gowiththegrow.R;
 
-public class AZFragment extends FragmentActivity implements AdapterView.OnClickListener{
+public class AZFragment extends AppCompatActivity implements AdapterView.OnClickListener{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plants);
+
+        String title = "Pflanzen von A-Z";
+        setTitle(title);
+
         TextView ananas = findViewById(R.id.ananas);
         ananas.setOnClickListener(this);
         TextView agave = findViewById(R.id.agave);
@@ -44,7 +51,7 @@ public class AZFragment extends FragmentActivity implements AdapterView.OnClickL
     public void onClick(View v) {
         // Then you start a new Activity via Intent
         Intent intent = new Intent();
-        intent.setClass(this.getApplicationContext(), DetailActivity.class);
+        intent.setClass(this.getBaseContext(), DetailActivity.class);
         startActivity(intent);
     }
 }
