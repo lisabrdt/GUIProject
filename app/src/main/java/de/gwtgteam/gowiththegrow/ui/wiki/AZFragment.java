a1b2c57030e4c216ae0c6d5cd27916cb32c053f6
Lisa Brundert <lisa.brundert@gmx.de>
2021-01-04 14:05:33 +0100

    added plants A-Z wiki

--- app/src/main/java/de/gwtgteam/gowiththegrow/ui/wiki/AZFragment.java
+++ app/src/main/java/de/gwtgteam/gowiththegrow/ui/wiki/AZFragment.java
@@ -2,25 +2,49 @@ package de.gwtgteam.gowiththegrow.ui.wiki;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.TextView;
 
 import androidx.fragment.app.FragmentActivity;
 
+import de.gwtgteam.gowiththegrow.DetailActivity;
 import de.gwtgteam.gowiththegrow.R;
 
-public class AZFragment extends FragmentActivity {
+public class AZFragment extends FragmentActivity implements AdapterView.OnClickListener{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plants);
-
-        Intent intent = getIntent();
-        int position = intent.getIntExtra("position", 0);
-
-        TextView myTextView = (TextView) findViewById(R.id.my_textview);
-        myTextView.setText("Hi");
+        TextView ananas = findViewById(R.id.ananas);
+        ananas.setOnClickListener(this);
+        TextView agave = findViewById(R.id.agave);
+        agave.setOnClickListener(this);
+        TextView aloevera = findViewById(R.id.aloevera);
+        aloevera.setOnClickListener(this);
+        TextView banane = findViewById(R.id.banane);
+        banane.setOnClickListener(this);
+        TextView basilikum = findViewById(R.id.basilikum);
+        basilikum.setOnClickListener(this);
+        TextView begonia = findViewById(R.id.begonia);
+        begonia.setOnClickListener(this);
+        TextView benjamini = findViewById(R.id.benjamini);
+        benjamini.setOnClickListener(this);
+        TextView calendula = findViewById(R.id.calendula);
+        calendula.setOnClickListener(this);
+        TextView chrysanthemen = findViewById(R.id.chrysanthemen);
+        chrysanthemen.setOnClickListener(this);
+        TextView dattelpalme = findViewById(R.id.dattelpalme);
+        dattelpalme.setOnClickListener(this);
 
     }
 
+    @Override
+    public void onClick(View v) {
+        // Then you start a new Activity via Intent
+        Intent intent = new Intent();
+        intent.setClass(this.getApplicationContext(), DetailActivity.class);
+        startActivity(intent);
+    }
 }
