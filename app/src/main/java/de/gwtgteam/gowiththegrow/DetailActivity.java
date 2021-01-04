0aa7a275b46fed20e87e9acb365faf68a99577b3
Vanessa <minf103166@fh-wedel.de>
2021-01-04 22:20:10 +0100

    Einstellungen

--- app/src/main/java/de/gwtgteam/gowiththegrow/DetailActivity.java
+++ app/src/main/java/de/gwtgteam/gowiththegrow/DetailActivity.java
@@ -7,22 +7,21 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
+    String title = "Ananas";
+    int img = R.drawable.plant_photo_7;
+    String family = "Bromeliengewächse";
+    String latinName = "Ananas comosus 'Variegatus'";
+    String light = "Mag es hell und braucht einige Stunden tägliche Sonnenbestrahlung";
+    String temp = "Braucht einen warmen Standort. Dieser sollte das gesamte Jahr über nicht unter 18 Grad Celsius fallen";
+    String water = "regelmäßig gießen und ein Austrocknen des Substrats verhindern. Aber aufgepasst: Staunässe verträgt die Ananas nicht.";
+    String humidity = "liebt eine hohe Luftfeuchtigkeit. Deshalb solltest du versuchen diese immer über 50% zu halten";
+    String dung = "bedarf nur weniger zusätzlicher Nährstoffe. Verwöhne deine Ananas von April bis August alle 14 Tage mit einer Dosis Flüssigdünger.";
+    String pest = "Grundsätzlich ist die Zierananas nicht sehr anfällig für Schädlinge und Krankheiten. Häufig sind Pflegefehler das Problem. Diese lassen jedoch leicht mit der richtigen Pflege beheben.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        String title = "Ananas";
-        int img = R.drawable.plant_photo_7;
-        String family = "Bromeliengewächse";
-        String latinName = "Ananas comosus 'Variegatus'";
-        String light = "Mag es hell und braucht einige Stunden tägliche Sonnenbestrahlung";
-        String temp = "Braucht einen warmen Standort. Dieser sollte das gesamte Jahr über nicht unter 18 Grad Celsius fallen";
-        String water = "regelmäßig gießen und ein Austrocknen des Substrats verhindern. Aber aufgepasst: Staunässe verträgt die Ananas nicht.";
-        String humidity = "liebt eine hohe Luftfeuchtigkeit. Deshalb solltest du versuchen diese immer über 50% zu halten";
-        String dung = "bedarf nur weniger zusätzlicher Nährstoffe. Verwöhne deine Ananas von April bis August alle 14 Tage mit einer Dosis Flüssigdünger.";
-        String pest = "Grundsätzlich ist die Zierananas nicht sehr anfällig für Schädlinge und Krankheiten. Häufig sind Pflegefehler das Problem. Diese lassen jedoch leicht mit der richtigen Pflege beheben.";
 
         setTitle(title);
         ImageView imageView = findViewById(R.id.detail_img);
